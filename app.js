c6f0c2ad45e4fd6060e00591e2a461938b1d8154
mbellucio <=>
2024-05-12 03:57:01 -0300

    Check event availability

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const apiURL = "http://127.0.0.1:3000";
+let numberToCurrency = new Intl.NumberFormat('pt-BR', {
+  style: 'currency',
+  currency: 'BRL',
+});
+
 
 const app = Vue.createApp({
   data() {
@@ -7,8 +12,12 @@ const app = Vue.createApp({
       searchText: "",
       screen: "buffets",
       current_buffet: {},
-      modal: true,
-      checkFeedback: ""
+      modal: false,
+      checkSituation: "",
+      checkFeedback: "",
+      checkErrors: [],
+      inputs: [],
+
     };
   },
 
@@ -45,10 +54,38 @@ const app = Vue.createApp({
       eventsData = await responseEvents.json();
       this.current_buffet = buffetData;
       this.current_buffet.events = eventsData;
+      this.current_buffet.events.forEach((event) => {
+        this.inputs.push({ bookingDate: "", guestNumber: 0 });
+        this.checkErrors.push({error: ""});
+      });
     },
 
     backToBuffets() {
       this.screen = "buffets";
+    },
+
+    closeModal() {
+      this.modal = false;
+    },
+
+    async checkAvailability(id, index) {
+      const {bookingDate, guestNumber} = this.inputs[index]
+      try {
+        this.checkErrors[index].error = ""
+        const params = new URLSearchParams({ booking_date: bookingDate, guest_number: guestNumber });
+        response = await fetch(`${apiURL}/api/v1/events/${id}?${params}`);
+        data = await response.json()
+        this.modal = true
+        if (data.error) {
+          this.checkFeedback = data.error
+          this.checkSituation = "Indisponível"
+          return
+        }
+        this.checkSituation = "Disponível"
+        this.checkFeedback = `Preço estimado de ${data.event} para ${guestNumber} pessoas: ${numberToCurrency.format(data.estimated_price)}`
+      } catch (error) {
+        this.checkErrors[index].error = "Preencha com dados válidos"
+      }
     },
   },
 });
